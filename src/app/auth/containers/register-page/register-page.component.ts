5565de33a8afdb98ecc8c37578d355c53c39fbc3
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-03-04 20:22:55 +0100

    Snackbar user existant

--- src/app/auth/containers/register-page/register-page.component.ts
+++ src/app/auth/containers/register-page/register-page.component.ts
@@ -3,23 +3,31 @@ import { RegisterFormComponent } from '../../components/register-form/register-f
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { IUserDto } from '../../../core/models/user.model';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [RegisterFormComponent],
+  imports: [RegisterFormComponent, MatSnackBarModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
 })
 export class RegisterPageComponent {
-  constructor(private router: Router, private userService: UserService) {}
+  constructor(private router: Router, private userService: UserService, private snackbar: MatSnackBar) {}
 
   public back(): void {
     this.router.navigateByUrl('/login');
   }
 
   public register(user: IUserDto): void {
-    this.userService.saveUser(user);
-    this.router.navigateByUrl('/home');
+    const alreadyExists = this.userService.getUserByEmail(user.email);
+    if(!alreadyExists) {
+      this.userService.saveUser(user);
+      this.router.navigateByUrl('/login');
+    } else {
+      this.snackbar.open('Utilisateur déja existant', 'Fermer', {
+        duration: 2000,
+      });
+    }
   }
 }
