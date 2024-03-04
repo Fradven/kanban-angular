98fd9476d7a4416254788b7f344249bc5a37945d
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-03-04 20:11:04 +0100

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { LoginPageComponent } from './auth/containers/login-page/login-page.component';
+import { BoardPageComponent } from './kanban/containers/board-page/board-page.component';
+import { RegisterPageComponent } from './auth/containers/register-page/register-page.component';
+import { HomePageComponent } from './kanban/containers/home-page/home-page.component';
+import { NotFoundPageComponent } from './core/containers/not-found-page/not-found-page.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'board', component: BoardPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: '**', component: NotFoundPageComponent },
+];
