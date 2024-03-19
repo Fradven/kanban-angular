3a53d0a1a901647e6a542818bc8cdb44fa101939
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-03-19 18:51:58 +0100

    Création d'un guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { BoardPageComponent } from './kanban/containers/board-page/board-page.co
 import { RegisterPageComponent } from './auth/containers/register-page/register-page.component';
 import { HomePageComponent } from './kanban/containers/home-page/home-page.component';
 import { NotFoundPageComponent } from './core/containers/not-found-page/not-found-page.component';
+import { authGuard } from './auth/guard/auth.guard';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegisterPageComponent },
-  { path: 'board', component: BoardPageComponent },
-  { path: 'home', component: HomePageComponent },
+  { path: 'board', component: BoardPageComponent, canActivate: [authGuard]},
+  { path: 'home', component: HomePageComponent, canActivate: [authGuard]},
   { path: '**', component: NotFoundPageComponent },
 ];
